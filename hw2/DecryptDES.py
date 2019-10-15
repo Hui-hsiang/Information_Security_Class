6c9b11cc2e1cc960a5ce2f76b1390da608dde13c
xianghw <fly305z102@gmail.com>
2019-10-15 23:12:21 +0800

    input_data process

--- hw2/DecryptDES.py
+++ hw2/DecryptDES.py
@@ -1,4 +1,10 @@
 import numpy as np
+import sys
 
-num = input 
-print (int(num,16))
+key = str(bin(int(sys.argv[1][2:],16)))[2:].zfill(56)
+key = np.asarray(list(map(int,key)))
+print (key,key.shape)
+cipyphertext = str(bin(int(sys.argv[2][2:],16)))[2:].zfill(64)
+cipyphertext = np.asarray(list(map(int,cipyphertext)))
+print(cipyphertext,cipyphertext.shape)
+
