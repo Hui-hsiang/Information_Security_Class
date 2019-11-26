c3e92d715022220dc8435f03ac0af0a69bd7cd2c
Hui-hsiang <48705017+Hui-hsiang@users.noreply.github.com>
2019-11-27 00:20:08 +0800

    no message

--- hw3/encrypt_ECB.py
+++ hw3/encrypt_ECB.py
@@ -33,30 +33,33 @@ def EBC_mode(key):
         f.close()
         output.close()
 # %%
-def EBC_mode(key):  
+def CBC_mode(key,iv):  
     try:
         f = open('myppm.ppm','rb')
-        output = open('EBC_encrypt.ppm','wb')
-        cipher_block = AES.new(key,AES.MODE_ECB)
+        output = open('CBC_encrypt.ppm','wb')
+        cipher_block = AES.new(key,AES.MODE_CBC)
         for i in range(3):
             data = f.readline()
             output.write(data)
             print(data)
         data = f.read(16)
-        i = int(0)
+        
         while data:
-            i += 1
             if len(data) < 16:
                 pd = 16 - len(data)
                 for i in range (pd):
                     data += bytes([0])
+            tmp = []
+            for d,i in zip(data,iv):
+                tmp = tmp.append(d^i)
+                
+            print (tmp)
+            data = bytes(tmp)
             cipher = cipher_block.encrypt(data)
-            if i > (12150 + 121500)/2:
-                output.write(cipher)
-            else:
-                output.write(data)
+            output.write(cipher)
+            iv = cipher
             data = f.read(16)
-        
+
     finally:
         f.close()
         output.close()
