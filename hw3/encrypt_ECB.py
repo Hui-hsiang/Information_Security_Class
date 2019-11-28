c7f299a61e9da279cbdb26b1a8b2578777f3d5b1
xianghw <fly305z102@gmail.com>
2019-11-28 23:43:14 +0800

    f

--- hw3/encrypt_ECB.py
+++ hw3/encrypt_ECB.py
@@ -59,9 +59,42 @@ def CBC_mode(key,iv):
     finally:
         f.close()
         output.close()
+#%%
+def Cool_mode(key,iv):  
+    try:
+        f = open('myppm.ppm','rb')
+        output = open('Cool_encrypt.ppm','wb')
+        cipher_block = AES.new(key,AES.MODE_ECB)
+        for i in range(3):
+            data = f.readline()
+            output.write(data)
+            print(data)
+        data = f.read(16)
+        while data:
+            if len(data) < 16:
+                pd = 16 - len(data)
+                for i in range (pd):
+                    data += bytes([0])
+            tmp = []
+            for d,i in zip(data,iv):
+                tmp.append((d ^ i) % 255)
+                
+            
+            data = bytes(tmp)
+            
+            cipher = cipher_block.encrypt(data)
+            output.write(cipher)
+            
+            tmp = iv[15:16]
+            tmp += iv[:15]
+            iv = bytes(tmp)
+            data = f.read(16)
 
+    finally:
+        f.close()
+        output.close()
 # %%
 EBC_mode(b'1234567887654321')
 CBC_mode(b'1234567887654321',b'11111111ffffffff')
-
+Cool_mode(b'1234567887654321',b'11111111ffffffff')
 # %%
